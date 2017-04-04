95a5f591eb868ffd734559d903b0f224dbafb00c
danxifuer <1073004628@qq.com>
2017-04-04 12:41:46 +0800

    commit

--- my_mxnet/my/train.py
+++ my_mxnet/my/train.py
@@ -1,10 +1,12 @@
 # import my_mxnet.my.googlenet as net
 import my_mxnet.my.googlenet as net
+import my_mxnet.my.config as cfg
 import my_mxnet.my.read_data as data
 from my_mxnet.my import fit
 import mxnet as mx
 
-sym = net.Symble(input={"data": mx.symbol.Variable(name="data")}, input_shape=(110, 3, 224, 224))
+sym = net.Symble(input={"data": mx.symbol.Variable(name="data")},
+                 input_shape=(cfg.batch_size / cfg.num_workers, 3, 224, 224))
 fit.fit(sym.layer['softmax'], data.get_rec_iter)
 
 
